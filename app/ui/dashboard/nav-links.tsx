79a24141d69f2dbc573713e5b3462623059037b9
Dexter <dtaylor13@live.com>
2024-04-30 00:01:28 -0400

    update prefetching

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -15,16 +15,19 @@ const links = [
     name: 'Home',
     href: '/dashboard',
     icon: HomeIcon,
+    prefetch: true,
   },
   {
     name: 'Invoices',
     href: '/dashboard/invoices',
     icon: DocumentDuplicateIcon,
+    prefetch: true,
   },
   {
     name: 'Customers',
     href: '/dashboard/customers',
     icon: UserGroupIcon,
+    prefetch: false,
   },
 ];
 
@@ -39,6 +42,7 @@ export default function NavLinks() {
           <Link
             key={link.name}
             href={link.href}
+            prefetch={link.prefetch}
             className={clsx(
               'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
               {
